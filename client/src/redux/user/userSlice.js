c008f530e6d92dddf6aa1d31f7586074bb3f9404
arjunvm1 <148052201+arjunvm1@users.noreply.github.com>
2024-03-20 11:39:29 +0530

    add delete user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,44 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    currentUser: null,
-    error: null,
-    loading: false,
-};
+  currentUser: null,
+  error: null,
+  loading: false,
+}
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        signInStart : (state) => {
-            state.loading=true;
-        },
-        signInSuccess: (state, action) => {
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error  = null;
-        },
-        signInFailure: (state,action) => {
-            state.error = action.payload;
-            state.loading = false;
-        },
-        updateUserStart: (state) => {
-            state.loading=true;
-        },
-        updateUserSuccess: (state,action) => {
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error = null;
-        },
-        updateUserFailure: (state, action) => {
-            state.error = action.payload;
-            state.loading = false;
-        },
-    }
-});
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload
+      state.loading = false
+      state.error = null
+    },
+    signInFailure: (state, action) => {
+      state.error = action.payload
+      state.loading = false
+    },
+    updateUserStart: (state) => {
+      state.loading = true
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload
+      state.loading = false
+      state.error = null
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload
+      state.loading = false
+    },
+    deleteUserStart: (state) => {
+      state.loading = true
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null
+      state.loading = false
+      state.error = null
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload
+      state.loading = false
+    },
+  },
+})
 
-export const {signInStart , signInSuccess, signInFailure, updateUserFailure, updateUserSuccess, updateUserStart } = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure, updateUserFailure, updateUserSuccess, updateUserStart, deleteUserFailure, deleteUserSuccess, deleteUserStart } = userSlice.actions
 
 // The value of `state` passed into the reducer is the state obtained by combining all reducers with their respective initial states through combine
 // The value of `reducer` in the Redux store will be set to the return value of this function.
-export default userSlice.reducer;
+export default userSlice.reducer
