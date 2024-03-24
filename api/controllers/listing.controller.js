3973c39a6a8c32d5364a9488fa5d1c512c0c21b0
arjunvm1 <148052201+arjunvm1@users.noreply.github.com>
2024-03-24 21:59:18 +0530

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -54,3 +54,17 @@ export const updateListing = async (req, res, next) => {
     next(error)
   }
 }
+
+
+//controller for getting a listing
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"))
+    }
+    res.status(200).json(listing)
+  } catch (error) {
+    next(error)
+  }
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,13 +1,16 @@
 import  express  from 'express';
-import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 
 router.post('/create', verifyToken, createListing); //route for creating a listing
+
 router.delete('/delete/:id',verifyToken, deleteListing) //route for deleting a listing
+
 router.post('/update/:id', verifyToken, updateListing) //route for updating  a listing
 
+router.get('/get/:id', getListing)// route for getting a listing
 
 export  default router;
