cbcf6c058e23a59f847fda32829cf4ea5faa47e3
arjunvm1 <148052201+arjunvm1@users.noreply.github.com>
2024-03-28 16:07:17 +0530

    add preinstallation

--- api/index.js
+++ api/index.js
@@ -5,13 +5,16 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('MongoDB connected');
     }).catch((err) => {
         console.log(err);
-    })
+    });
+
+    const __dirname = path.resolve();
 
 const app = express();
 
@@ -22,9 +25,19 @@ app.listen(3000,() =>{
     console.log("server is running on port 3000!!!");
 })
 
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
-app.use('/api/listing',listingRouter)
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+app.use('/api/listing',listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+
+res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+
+})
+
 
 app.use((err,req,res,next) =>{
     const statusCode = err.statusCode || 500;
